01c77764e2718be38d7faedafbed7a123d87e388
deivis narea sevilla <144132424+deivisNS@users.noreply.github.com>
2024-03-22 09:45:08 -0400

    Update cargar_archivos.py

--- modulos/cargar_archivos.py
+++ modulos/cargar_archivos.py
@@ -22,7 +22,7 @@ class Cargar():
 		for archivo in lista:
 
 			#preguntamos por la extencion del archivo
-			if archivo.endswith(".png") or archivo.endswith(".jpeg"):
+			if archivo.endswith(".png") or archivo.endswith(".jpg"):
 
 				#preguntamos por el tamaño del archivo
 				if os.path.getsize(archivo) >= int(tamanno):

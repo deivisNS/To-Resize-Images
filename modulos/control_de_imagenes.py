44c18f893db74c762e683856b77bd52854cea231
deivis narea sevilla <144132424+deivisNS@users.noreply.github.com>
2024-05-16 10:50:27 -0400

    Update control_de_imagenes.py

--- modulos/control_de_imagenes.py
+++ modulos/control_de_imagenes.py
@@ -9,34 +9,33 @@ import os  #para interactuar con el sistema operativo
 extencion_guardado > la extencion con la que se guardara la imagen"""
 def tamanno(imagen_grande, dimensiones, extencion_guardado, guardar_en):
 
-	nombre, extencion = os.path.splitext(imagen_grande)
+    nombre, extencion = os.path.splitext(imagen_grande)
 
-	try:
-	    with Image.open(imagen_grande) as imagen:		
+    try:
+	        with Image.open(imagen_grande) as imagen:		
 
-		    #si es solo un entero
-		    if int(dimensiones):
-					
-			    #tomamos el ancho y largo de la imagen
-			    width, height = imagen.size  
+		        #si es solo un entero
+		        if int(dimensiones):
+			       
+				    #tomamos el ancho y largo de la imagen
+			        width, height = imagen.size  
 				
-			    #reducimos el ancho
-			    width = width * dimensiones // 100
+			        #reducimos el ancho
+			        width = width * dimensiones // 100
 					
-			    #reducimos el largo
-			    height = height * dimensiones // 100
+			        #reducimos el largo
+			        height = height * dimensiones // 100
 
-			    #creamos una nueva imagen pero con las reducciones hechas
-			    nueva_imagen = imagen.resize((width, height), Image.ANTIALIAS)
+			        #creamos una nueva imagen pero con las reducciones hechas
+			        nueva_imagen = imagen.resize((width, height), Image.ANTIALIAS)
 
-
-			    if extencion_guardado == "jpeg":
+			        if extencion_guardado == "jpeg":
 				
-			        nueva_imagen = nueva_imagen.convert("RGB")
-	
+			            nueva_imagen = nueva_imagen.convert("RGB")
 
-			    #guardamos la imagen
-			    nueva_imagen.save(f"{guardar_en}/{nombre}.{extencion_guardado}", extencion_guardado.upper())
+	    #guardamos la imagen
+	    nueva_imagen.save(f"{guardar_en}/{nombre}.{extencion_guardado}", extencion_guardado.upper())
+
     except:
         print(imagen_grande)
 

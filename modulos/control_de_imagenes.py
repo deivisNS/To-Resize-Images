43759545a9069ff65c0a172ed4fd4eb4c3e7d7b2
deivis narea sevilla <144132424+deivisNS@users.noreply.github.com>
2024-05-17 19:57:32 -0400

    Update control_de_imagenes.py

--- modulos/control_de_imagenes.py
+++ modulos/control_de_imagenes.py
@@ -3,41 +3,38 @@ import os  #para interactuar con el sistema operativo
 
 
 
-
-
 """dimensiones > lista con el ancho y largo deseado o un entero que representara un porcentaje
 extencion_guardado > la extencion con la que se guardara la imagen"""
 def tamanno(imagen_grande, dimensiones, extencion_guardado, guardar_en):
 
-    nombre, extencion = os.path.splitext(imagen_grande)
+	nombre, extencion = os.path.splitext(imagen_grande)
 
-    try:
-	        with Image.open(imagen_grande) as imagen:		
+	try:
 
-		        #si es solo un entero
-		        if int(dimensiones):
-			       
-				    #tomamos el ancho y largo de la imagen
-			        width, height = imagen.size  
-				
-			        #reducimos el ancho
-			        width = width * dimensiones // 100
-					
-			        #reducimos el largo
-			        height = height * dimensiones // 100
+		with Image.open(imagen_grande) as imagen:
 
-			        #creamos una nueva imagen pero con las reducciones hechas
-			        nueva_imagen = imagen.resize((width, height), Image.ANTIALIAS)
+			#si es solo un entero
+			if int(dimensiones):
 
-			        if extencion_guardado == "jpeg":
-				
-			            nueva_imagen = nueva_imagen.convert("RGB")
+				#tomamos el ancho y largo de la imagen
+				width, height = imagen.size
+	
+				#reducimos el ancho
+				width = width * dimensiones // 100
 
-	    #guardamos la imagen
-	    nueva_imagen.save(f"{guardar_en}/{nombre}.{extencion_guardado}", extencion_guardado.upper())
+				#reducimos el largo
+				height = height * dimensiones // 100
 
-    except:
-        print(imagen_grande)
+				#creamos una nueva imagen pero con las reducciones hechas
+				nueva_imagen = imagen.resize((width, height), Image.ANTIALIAS)
+
+				if extencion_guardado == "jpeg":
+
+					nueva_imagen = nueva_imagen.convert("RGB")
+
+				#guardamos la imagen
+				nueva_imagen.save(f"{guardar_en}/{nombre}.{extencion_guardado}", extencion_guardado.upper())
 
 
-		
+	except:
+		print(imagen_grande)
